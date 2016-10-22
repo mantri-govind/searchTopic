74d08ea9a85951962fb6efd1b676e5e33b5fadb5
Govind Mantri <mantri.govind@hotmail.com>
2016-10-22 23:07:05 +0530

    navbar added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-
+ def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 protected
