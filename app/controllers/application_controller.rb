8d88e8eacadbdf2ea648edbfb008dafb95e61735
Govind <ggovind.gopal@tcs.com>
2016-10-25 18:29:06 +0530

    Changes after Setting Current User In Devise.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,17 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
- def current_user
-    return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
-  end
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-protected
-
-def configure_permitted_parameters
-  devise_parameter_sanitizer.for(:sign_up) << :name
-  devise_parameter_sanitizer.for(:account_update) << :name
-end
 
 
-  protect_from_forgery with: :exception
+ before_filter :authenticate_user!
+
+# protected
+
+# def configure_permitted_parameters
+#   devise_parameter_sanitizer.for(:sign_up) << :name
+#   devise_parameter_sanitizer.for(:account_update) << :name
+# end
+
+
+#   protect_from_forgery with: :exception
 end
